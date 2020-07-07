3fd15ed6f4fbb412cd3552fcdec5d1e8e2f5d46e
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-07-07 11:48:49 -0400

    added media queries and animation

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+    // slide animation to the section when respective section link is clicked
+    const scrollToSection = (sectionId) => {
+        let section = $(`section[id='${sectionId}']`);
+        $('html,body').animate({scrollTop: section.offset().top},'slow');
+    }
+    
+    $("#about-link").on('click', function() {
+       scrollToSection('about');
+    });
+
+    $("#skills-link").on('click', function() {
+       scrollToSection('skills');
+    });
+
+    $("#projects-link").on('click', function() {
+       scrollToSection('projects');
+    });
+
+    $("#contact-link").on('click', function() {
+       scrollToSection('contact');
+    });
+
+    // download cv button functionality
     $('#resume').on('click', function(e) {
         e.preventDefault();
         window.open(
@@ -8,6 +31,20 @@ $(document).ready(function(){
         ) 
     })
 
+    // trigger the animation on the sections
+    $(window).scroll(function () {
+        $('section').each(function () {
+            let elementPosition = $(this).offset().top;
+            let elementHeight = $(this).height();
+            let topOfWindow = $(window).scrollTop();
+    
+            if (elementPosition < topOfWindow + elementHeight && elementPosition + elementHeight > topOfWindow) {
+                $(this).addClass("animate");
+            } 
+        });
+    });
+
+    // links to the github repos and live host of the projects
     $('#numberlicios-github').on('click', function() {
         window.open(
             'https://github.com/oleglzvt/numberlicious',
@@ -50,6 +87,7 @@ $(document).ready(function(){
         ) 
     })
 
+    // go back button after submitting the contact form
     $('#go-back').on('click', function() {
         window.location.href = './index.html'
     })
